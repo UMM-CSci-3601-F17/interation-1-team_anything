8bc00305a78f2345f59c103ac47981d291ea4f1e
Vipul Sharma <vipulsharma936@gmail.com>
2017-10-09 22:57:41 -0500

    Added the CardController class to /sage/card/.

--- server/src/main/java/umm3601/Deck/Deck.java
+++
@@ -1,7 +0,0 @@
-package umm3601.Deck;
-
-import org.bson.types.ObjectId;
-public class Deck {
-    String name;
-    String cardObjectID = [ObjectId _id = new ObjectId()];
-}
---
+++ server/src/main/java/umm3601/sage/deck/DeckController.java
@@ -0,0 +1,4 @@
+package umm3601.sage.deck;
+
+public class DeckController {
+}
