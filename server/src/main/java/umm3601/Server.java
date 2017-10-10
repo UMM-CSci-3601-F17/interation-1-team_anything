4e08474dc3b921f964c5385ba6e0e1020525befa
Vipul Sharma <vipulsharma936@gmail.com>
2017-10-10 04:57:53 -0500

    Added routes to deck, card id and deck id in the Server class.

--- server/src/main/java/umm3601/Server.java
+++ server/src/main/java/umm3601/Server.java
@@ -8,6 +8,9 @@ import spark.Route;
 import spark.utils.IOUtils;
 import umm3601.user.UserController;
 
+import umm3601.sage.card.CardController;
+import umm3601.sage.deck.DeckController;
+
 import java.io.IOException;
 import java.io.InputStream;
 
@@ -15,15 +18,17 @@ import static spark.Spark.*;
 import static spark.debug.DebugScreen.enableDebugScreen;
 
 public class Server {
-    private static final String userDatabaseName = "dev";
+    private static final String databaseName = "i1-droptable-dev";
     private static final int serverPort = 4567;
 
     public static void main(String[] args) throws IOException {
 
         MongoClient mongoClient = new MongoClient();
-        MongoDatabase userDatabase = mongoClient.getDatabase(userDatabaseName);
+        MongoDatabase database = mongoClient.getDatabase(databaseName);
+        UserController userController = new UserController(database);
 
-        UserController userController = new UserController(userDatabase);
+        CardController cardController = new CardController(database);
+        DeckController deckController = new DeckController(database);
 
         //Configure Spark
         port(serverPort);
@@ -72,6 +77,11 @@ public class Server {
         get("api/users", userController::getUsers);
         get("api/users/:id", userController::getUser);
         post("api/users/new", userController::addNewUser);
+
+
+        get("api/decks", deckController::getDecks);
+        get("api/cards/:id", cardController::getCard);
+        get("api/decks/:id", deckController::getDeck);
 
         // An example of throwing an unhandled exception so you can see how the
         // Java Spark debugger displays errors like this.
