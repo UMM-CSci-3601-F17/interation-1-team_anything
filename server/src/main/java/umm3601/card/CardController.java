8f5d3f22e1ffb2c0140dcdd070d00f052f986c73
vipulroxx <sharm493@morris.umn.edu>
2017-10-10 16:58:02 -0500

    Added getCards to CardController

--- server/src/main/java/umm3601/card/CardController.java
+++ server/src/main/java/umm3601/card/CardController.java
@@ -1,6 +1,8 @@
 package umm3601.card;
 
 import com.google.gson.Gson;
+import com.mongodb.*;
+import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 import com.mongodb.util.JSON;
@@ -13,6 +15,7 @@ import java.util.Iterator;
 import java.util.Map;
 
 import static com.mongodb.client.model.Filters.eq;
+
 
 public class CardController {
 
@@ -62,6 +65,73 @@ public class CardController {
             // We didn't find the desired deck
             return null;
         }
+    }
+
+    public String getCards(Request req, Response res)
+    {
+        res.type("application/json");
+        return getCards(req.queryMap().toMap());
+    }
+
+    /**
+     * @param queryParams
+     * @return an array of Users in a JSON formatted string
+     */
+    public String getCards(Map<String, String[]> queryParams) {
+
+        Document filterDoc = new Document();
+
+        if (queryParams.containsKey("word")) {
+            String targetWord = (queryParams.get("word")[0]);
+            filterDoc = filterDoc.append("word", targetWord);
+        }
+
+        if (queryParams.containsKey("synonym")) {
+            String targetSynonym = (queryParams.get("synonym")[0]);
+            filterDoc = filterDoc.append("synonym", targetSynonym);
+        }
+
+        if (queryParams.containsKey("antonym")) {
+            String targetAntonym = (queryParams.get("antonym")[0]);
+            filterDoc = filterDoc.append("antonym", targetAntonym);
+        }
+
+        if (queryParams.containsKey("general_sense")) {
+            String targetGeneralization = (queryParams.get("general_sense")[0]);
+            filterDoc = filterDoc.append("general_sense", targetGeneralization);
+        }
+
+        if (queryParams.containsKey("example_usage")) {
+            String targetExamples = (queryParams.get("example_usage")[0]);
+            filterDoc = filterDoc.append("example_usage", targetExamples);
+        }
+
+
+        //FindIterable comes from mongo, Document comes from Gson
+        Iterable<Document> matchingCards = cardCollection.find(filterDoc);
+
+        return JSON.serialize(matchingCards);
+    }
+
+    public boolean addNewCard(String word, String synonym, String antonym, String general, String example) {
+
+        Document newCard = new Document();
+        newCard.append("word", word);
+        newCard.append("synonym", synonym);
+        newCard.append("antonym", antonym);
+        newCard.append("general", general);
+        newCard.append("example", example);
+
+        try {
+            cardCollection.insertOne(newCard);
+        }
+        catch(MongoException ree)
+        {
+            ree.printStackTrace();
+            return false;
+        }
+
+        return true;
     }
 
 }
